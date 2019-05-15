b92a1d3b4b7ee7da4eaf3364736511e69e1f8c4e
Flobodan <43610431+Flobodan@users.noreply.github.com>
2019-05-15 13:01:16 +0200

    Cleanup

--- got.java
+++ got.java
@@ -5,25 +5,23 @@ class got{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int UserInput = in.nextInt();
-        int OldUserInput;
         while (UserInput != 1){       
             //Check ob die Zahl durch 3 teilbar ist
             if (UserInput%3 == 0){
-                OldUserInput = UserInput;
-                UserInput = UserInput/3;
-                System.out.println(OldUserInput + " 0");
+                System.out.println(UserInput + " 0");
+                UserInput = UserInput/3;                
             //wenn nicht dann ob die zahl +1 durch 3 teilbar ist
             }else if((UserInput + 1) % 3==0){
-                OldUserInput = UserInput;
-                UserInput = (UserInput + 1)/3;
-                System.out.println(OldUserInput + " +1");
+                System.out.println(UserInput + " +1");
+                UserInput = (UserInput + 1)/3;               
             //wenn nicht dann ob die zahl -1 durch 3 teilbar ist
             }else if ((UserInput - 1) % 3==0){
-                OldUserInput = UserInput;
+                System.out.println(UserInput + " -1");
                 UserInput = (UserInput - 1)/3;
-                System.out.println(OldUserInput + " -1");
+                
             }
         }
+        in.close();
         
     }
 }
